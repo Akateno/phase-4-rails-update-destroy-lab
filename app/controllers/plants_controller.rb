410b7cf7ce3b14b8feadc2e9fbccb1dde9bd1d9c
Erick Vargas <erickkvargas@gmail.com>
2022-06-07 15:09:46 -0700

    commit tree c3d3d4eae3ece6f695369ba68e480ce10bc0254c

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,22 +1,37 @@
 class PlantsController < ApplicationController
 
-  # GET /plants
-  def index
-    plants = Plant.all
-    render json: plants
-  end
+ # GET /plants
+ def index
+  plants = Plant.all
+  render json: plants
+end
 
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
+# GET /plants/:id
+def show
+  plant = Plant.find_by(id: params[:id])
+  render json: plant
+end
 
-  # POST /plants
-  def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
-  end
+# POST /plants
+def create
+  plant = Plant.create(plant_params)
+  render json: plant, status: :created
+end
+
+# PATCH /plants/:id
+def update
+  plant = Plant.find_by(id: params[:id])
+  plant.update(plant_params)
+  render json: plant
+end
+
+# DELETE /plants/:id
+def destroy
+  plant = Plant.find_by(id: params[:id])
+  plant.destroy
+  head :no_content
+end
+
 
   private
 
